c160660ea3be7a74ccf6c01238c5190b92955397
Castro081 <ivancastro02.ic@gmail.com>
2025-03-31 20:42:00 -0600

    add templates and css

--- arqui_wifi/settings.py
+++ arqui_wifi/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Modulos'
 ]
 
 MIDDLEWARE = [
@@ -117,6 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Si tienes una carpeta "static" en la raíz del proyecto
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
