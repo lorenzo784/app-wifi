cf04659d1d76a24266a3bd227a0051916e353c32
Lorenzo López <lorenzolpz784@gmail.com>
2025-04-02 00:43:01 -0600

    fix stuff

--- arqui_wifi/settings.py
+++ arqui_wifi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gio4&_%q5i8pd2t0if=i50s8e4m0&4e@citsdgy%(qg#5vi1^@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,11 +118,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",  # Si tienes una carpeta "static" en la raíz del proyecto
-]
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'staticfiles' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- arqui_wifi/urls.py
+++ arqui_wifi/urls.py
@@ -28,4 +28,4 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
